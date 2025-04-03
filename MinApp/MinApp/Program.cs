acbf23fd5b9a4dfaef569e2ca18160a1ffe8666c
keiakihito <kkatsumi.git@gmail.com>
2025-04-03 12:25:25 -0700

    Adding docker enable to hot reload for dev

--- MinApp/MinApp/Program.cs
+++ MinApp/MinApp/Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Add explicitly hort number to avoid troubles
+builder.WebHost.UseUrls("http://*:8080");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
